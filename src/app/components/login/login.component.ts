d83a6d8eb2b865b312237191215968e80e5d6193
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-07-12 15:33:36 -0300

    feat: html and css login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public iconPassword: string = 'visibility';
+  public typePassword: string = 'password';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public visiblePassword(): void {
+    if(this.iconPassword === 'visibility') {
+      this.iconPassword = 'visibility_off';
+      this.typePassword = 'text';
+    } else {
+      this.iconPassword = 'visibility';
+      this.typePassword = 'password';
+    }
   }
 
 }
--- src/app/shared/app-material/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material/app-material.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   exports: [
     MatSidenavModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AppMaterialModule { }
