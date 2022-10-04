0a46e79753f4363031f748523f908012bcbf6441
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-10-04 18:09:13 -0300

    feat: page setting responsive

--- src/app/shared/components/settings/settings.component.ts
+++ src/app/shared/components/settings/settings.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  public formUser = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.max(50)]],
+    lastName: ['', [Validators.required, Validators.max(50)]],
+    newPassword: ['', [Validators.required, Validators.max(50)]],
+    repeatPassword: ['', [Validators.required, Validators.max(50)]]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  public submit(): void{
+
+  }
+
+  public cancel(): void {
+
+  }
 }
